af60ce333b5293fb3bb8d9df46e5caf517f92245
Katrina Finney <katrinafinney@gmail.com>
2025-04-03 23:31:16 -0400

    polish

--- src/app/components/CallToAction.tsx
+++ src/app/components/CallToAction.tsx
@@ -21,7 +21,7 @@ export default function CallToAction() {
           Breathe New Life into Your Website
         </h2>
         <p className={styles.subtext}>
-          Whether you're facing a sudden outage or need a long-overdue upgrade, we've got you covered. Let’s work together to restore and enhance your site—so you can get back to what matters most.
+          Whether you&apos;re facing a sudden outage or need a long-overdue upgrade, we&apos;ve got you covered. Let&rsquo;s work together to restore and enhance your site&mdash;so you can get back to what matters most.
         </p>
         <button className={styles.button} onClick={handleClick}>
           Stabilize My Site
