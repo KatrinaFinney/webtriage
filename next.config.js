b9b778fec9d750be3e550157caec8673a58ce839
Katrina Finney <katrinafinney@gmail.com>
2025-04-30 10:18:09 -0400

    free scan debugging

--- next.config.js
+++ next.config.js
@@ -1,25 +1,22 @@
 // next.config.js
-const webpack = require('webpack');
-
+const webpack = require('webpack')
 /** @type {import('next').NextConfig} */
 module.exports = {
   webpack(config, { isServer }) {
     if (isServer) {
-      // 1) Treat these as externals so runtime require() works
-      config.externals = [
-        ...(Array.isArray(config.externals) ? config.externals : [config.externals]),
+      // 1) Preserve Next.js defaults, then append
+      config.externals.push(
         'chrome-aws-lambda',
         'lighthouse',
         'puppeteer-core',
-      ];
-
-      // 2) Ignore any dynamic requires under chrome-aws-lambda/build
+      )
+      // 2) If you still need to ignore dynamic chrome-aws-lambda files:
       config.plugins.push(
         new webpack.IgnorePlugin({
           resourceRegExp: /chrome-aws-lambda[\/\\]build/,
         })
-      );
+      )
     }
-    return config;
+    return config
   },
-};
+}
